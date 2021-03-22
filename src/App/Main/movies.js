7bfb9b8f5dc295883cec849223d78bace4f64cdc
olexii <omedvedew@ukr.net>
2021-03-22 13:20:39 +0200

    added movies images

--- src/App/Main/movies.js
+++ src/App/Main/movies.js
@@ -9,25 +9,25 @@ const movies = [
         id: 2,
         title: 'Lord of the Rings: The Two Towers',
         description: 'Second part of famous trilogy "The Lord Of The Rings"',
-        image: '',
+        image: 'two-towers.jpg',
     },
     {
         id: 3,
         title: 'Lord of the Rings: Return of the King',
         description: 'Third part of famous trilogy "The Lord Of The Rings"',
-        image: '',
+        image: 'return-of-the-king.jpg',
     },
     {
         id: 4,
         title: 'Angels and Demons',
         description: 'First part of famous novel and movie series narrating the story about Robert Langdon`s adventures',
-        image: '',
+        image: 'angels-and-demons.jpg',
     },
     {
         id: 5,
         title: 'The Da Vinci Code',
         description: 'Second part of famous novel and movie series narrating the story about Robert Langdon`s adventures',
-        image: '',
+        image: 'da-vinci-code.jpg',
     },
 ];
 
